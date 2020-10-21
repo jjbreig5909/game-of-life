18b9c94dc343abd1f994bacf5ef53c009c52615b
Jeff Breig <jjbreig@gmail.com>
2020-10-21 15:18:15 -0400

    grid set at 25x25, modified to allow neighbor wrapping, added speed slider

--- game-of-life/src/components/Grid.js
+++ game-of-life/src/components/Grid.js
@@ -1,90 +1,181 @@
-import React, {useCallback, useState, useEffect, useRef} from "react";
-import produce from 'immer';
+import React, { useState, useCallback, useRef, useEffect } from 'react';
+import Slider from '@material-ui/core/Slider'
+import produce from 'immer'; //Immer is used for "double-buffering", see "produce" function below
 
-function Grid(){
-    // Setting initial Grid Size
-    startingSize = {
-        rows: 25,
-        columns: 25,
-    }
-
-    const operations = [
-        [0, 1],
-        [0, -1],
-        [1, -1],
-        [-1, 1],
-        [1, 1],
-        [-1, -1],
-        [1, 0],
-        [-1, 0],
-      ];
-    
-    // Declaring variables
-    const [gridSize, setGridSize] = useState(startingSize);
-    const [running, setRunning] = useState(false);
-    const runningRef = useRef(running);
-    runningRef.current = running
-    const [generation, setGeneration] = useState(0);
-    const [sum, setSum] = useState(0);
-    const timeRef=useRef(300);
-    const [grid, setGrid] = useState(startingSize)
-    const gridRows = 25;
-    const gridColumns = 25;
-    
-
-    function initialState() {
-        const rows = [];
-        for( let i=0; i < gridRows; i++) {
-            rows.push(Array.from(Array(numCols), () => 0));
-        }
-        return rows;
-    }
-
-    const playGame = useCallback(() => {
-        if (!runningRef.current) {
-            return;
-        }
-        setGrid((g) => {
-            let validGrid = false;
-            validGrid = false;
-            return produce(g, (gridCopy) =>{
-                for(let i = 0; i<gridRows; i++) {
-                    for(let j=0; j<gridColumns; i++) {
-                        let neighbors = 0;
-                        operations.forEach(([x,y]) => {
-                            const newI = i+x;
-                            const newJ = j+y;
-                            if(newI >= 0 && newI < gridRows && newJ >= 0 && newJ < numCols) {
-                                neighbors += g[newI][newJ];
-                            }
-                        });
-                        if(neighbors<2 || neighbors > 3) {
-                            gridCopy[i][j] = 0;
-                        } else if (g[i][j] === 0 && neighbors === 3) {
-                            validGrid = true;
-                            gridCopy[i][j] = 1;
-                        }
-                    }
-                }
-                if(validGrid){
-                    setGeneration((num)=>(num+1));
-                }
-                setSum(
-                    gridCopy.flat().reduce((acc,cv) =>{
-                        return acc + cv
-                    })
-                )
-            })
-        })
-        setTimeout(playGame, timeRef.current);
-
-    }, [timeRef.current]);
-    
-    return(
-        <div>
-            Hello World
-        </div>
-    )
+const initialSize = { 
+  numRows: 25,
+  numCols: 25,
 }
 
+const possibleNeighbors = [
+  [0, 1],
+  [0, -1],
+  [1, -1],
+  [-1, 1],
+  [1, 1],
+  [-1, -1],
+  [1, 0],
+  [-1, 0],
+];
+
+function Grid() {
+
+  const [running, setrunning] = useState(false);
+  const [generation, setGeneration] = useState(0);
+  const [sum, setSum] = useState(0);
+  const runningRef = useRef(running);
+  runningRef.current = running;
+  const timeRef = useRef(300);
+  const numRows = 25;
+  const numCols = 25;
+  const [grid, setGrid] = useState(initialState);
+
+  function initialState() {
+    const rows = [];
+    for (let i = 0; i < numRows; i++) {
+      rows.push(Array.from(Array(numCols), () => 0));
+    }
+    return rows;
+  }
+
+  function handleSliderChange(e, newValue){
+    timeRef.current = 1000-newValue;
+  }
+
+  
+  
+  const playGame = useCallback(() => {
+    if (!runningRef.current) { //Checks to see if game is running
+      return;
+    }
+    setGrid((g) => {
+      let newGen = false;
+      newGen = false;
+      return produce(g, (gridCopy) => { //Using immer for double buffering
+        for (let i = 0; i < numRows; i++) { //These for loops iterate through each cell of the grid
+          for (let k = 0; k < numCols; k++) {
+            let neighbors = 0;
+            possibleNeighbors.forEach(([x, y]) => { //Checking for status of neighbors for each cell, board wraps around edges
+              const newI = (i + x +numRows) % numRows; 
+              const newK = (k + y + numCols) % numCols;
+              if (newI >= 0 && newI < numRows && newK >= 0 && newK < numCols) {
+                neighbors += g[newI][newK];
+              }
+            });
+            if (neighbors < 2 || neighbors > 3) { //These are the rules of the game.
+              gridCopy[i][k] = 0;
+            } else if (g[i][k] === 0 && neighbors === 3) {
+              newGen = true;
+              gridCopy[i][k] = 1;
+            }
+          }
+        }
+        if(newGen) {
+          setGeneration((num) => (num + 1)); //Iterating each generation number
+        }
+        setSum(
+          gridCopy.flat().reduce((total, cell) => { //Adding up the cell populations
+            return total + cell
+          })
+        )
+      });
+    });
+    setTimeout(playGame, timeRef.current);
+  }, [timeRef.current]);
+
+  return (
+    <div className = 'container'>
+        <div className = 'buttons'>
+      <button
+        onClick={() => {
+          setrunning(!running);
+          if (!running) {
+            runningRef.current = true;
+            playGame();
+          }
+        }}
+      >
+        {!runningRef.current ? 'Start' : 'Stop'}
+      </button>
+      <button
+        onClick={() => {
+          const rows = [];
+          for (let i = 0; i < numRows; i++) {
+            rows.push(
+              Array.from(Array(numCols), () => (Math.random() > 0.75 ? 1 : 0))
+            );
+          }
+          setGrid(rows);
+          setSum(
+            grid.flat().reduce((total, cell) => { //Adding up the cell populations
+              return total + cell
+            })
+          )
+        }}
+      >
+        Random
+      </button>
+      <button
+        onClick={() => {
+          setGrid(initialState);
+          setGeneration(0);
+          setSum(0);
+        }}
+      >
+        Reset
+      </button>
+      </div>
+      <div className = 'spacer'>
+
+      </div>
+      <div className = 'slider'>
+        <Slider 
+        defaultValue={300}
+        onChange = {handleSliderChange}
+        aria-labelledby="discrete-slider-small-steps"
+        min={1}
+        max={1000}
+        valueLabelDisplay="auto"
+        />
+      </div>
+      <div className = 'grid'
+        style={{
+          display: 'grid',
+          gridTemplateColumns: `repeat(${numCols},15px)`,
+        }}
+      >
+        {grid.map((rows, i) =>
+          rows.map((col, k) => (
+            <div
+              key={`${i}-${k}`}
+              onClick={() => {
+                const newGrid = produce(grid, (gridCopy) => {
+                  gridCopy[i][k] = grid[i][k] ? 0 : 1;
+                });
+                setGrid(newGrid);
+              }}
+              style={{ 
+                width: 14,
+                height: 15,
+                backgroundColor:
+                grid[i][k] && sum > 125
+                  ? 'green'
+                  : grid[i][k] && sum > 85
+                  ? 'blue'
+                  : grid[i][k] && sum > 50
+                  ? 'purple'
+                  : grid[i][k] && sum >= 0
+                  ? 'red'
+                  : undefined,
+                border: 'solid 1.5px black',
+              }}
+            />
+          ))
+        )}
+      </div>
+        <p className='info'> Generation: {generation}</p>
+        <p className='info'> Cell Population: {sum}</p>
+    </div>
+  );
+}
 export default Grid;
